70d724e18ddbf8d13f29c380964ddac99f3ba097
suyimin <suyimin2001@163.com>
2018-01-23 18:00:16 +0800

    初始提交。

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_com_example_ndk_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    __android_log_print(ANDROID_LOG_DEBUG, "zj", "%s", "Hello, World");
+    return env->NewStringUTF(hello.c_str());
+}
