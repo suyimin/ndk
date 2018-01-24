7bed8a74a9dd3551c75e5b56c4702927ec00e01b
suyimin <suyimin2001@163.com>
2018-01-24 10:42:57 +0800

    增加纯C++(不含java)的应用Demo:native。

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,9 +6,7 @@ extern "C"
 JNIEXPORT jstring
 
 JNICALL
-Java_com_example_ndk_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
+Java_com_example_ndk_MainActivity_stringFromJNI(JNIEnv *env, jobject) {
     std::string hello = "Hello from C++";
     __android_log_print(ANDROID_LOG_DEBUG, "zj", "%s", "Hello, World");
     return env->NewStringUTF(hello.c_str());
